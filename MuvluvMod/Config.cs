20cd11dbb75523bfd683ca084e0e938965dff0ee
DDXDB <984400286@qq.com>
2026-03-10 18:59:52 +0800

    cdn

--- MuvluvMod/Config.cs
+++ MuvluvMod/Config.cs
@@ -18,11 +18,11 @@ namespace MuvluvMod
         {
             DynamicMosaic = Plugin.Config.Bind("Gerneral", "DynamicMosaic", false, "是否开启游戏内动态马赛克");
             EnableSkipButton = Plugin.Config.Bind("Gerneral", "EnableSkipButton", false, "是否总是开启跳过按钮");
-            VoiceInterruption = Plugin.Config.Bind("Gerneral", "VoiceInterruption", true, "剧情中播放下一句话时是否中断当前语音");
+            VoiceInterruption = Plugin.Config.Bind("Gerneral", "VoiceInterruption", false, "剧情中播放下一句话时是否中断当前语音");
             AutoSkipBattle = Plugin.Config.Bind("Gerneral", "AutoSkipBattle", false, "自动跳过战斗（自动按跳过键，不受跳过键开关影响）");
 
             Translation = Plugin.Config.Bind("Translation", "Enable", true, "是否开启汉化");
-            TranslationCDN = Plugin.Config.Bind("Translation", "CdnURL", "https://raw.githubusercontent.com/anosu/muvluvgg-translation/refs/heads/main", "翻译加载的CDN");
+            TranslationCDN = Plugin.Config.Bind("Translation", "CdnURL", "https://raw.githubusercontent.com/DDXDB/MGG-AGT-ZH/refs/heads/Text_Repository", "翻译加载的CDN");
             FontBundlePath = Plugin.Config.Bind("Translation", "FontBundlePath", "font/sarasagothicsc-bold", "TMP字体AssetBundle的路径");
             FontAssetName = Plugin.Config.Bind("Translation", "FontAssetName", "SarasaGothicSC-Bold SDF", "AssetBundle中TMP_FontAsset的名称");
 
